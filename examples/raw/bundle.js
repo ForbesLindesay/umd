f5fd0e6cfd83bc954334e6c08f899a9b67dff563
Domenic Denicola <domenic@domenicdenicola.com>
2013-07-29 00:21:24 -0400

    Get rid of SES and Montage bootstrap.
    As per kriskowal/q#317 discussions.

--- examples/raw/bundle.js
+++ examples/raw/bundle.js
@@ -1,3 +1,3 @@
-(function(e){if("function"==typeof bootstrap)bootstrap("common-js-module",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeCommonJsModule=e}else"undefined"!=typeof window?window.commonJsModule=e():global.commonJsModule=e()})(function(){var define,ses,bootstrap,module,exports;
+!function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.commonJsModule=e():"undefined"!=typeof global?global.commonJsModule=e():"undefined"!=typeof self&&(self.commonJsModule=e())}(function(){var define,module,exports;
 return "Raw example";
 });
