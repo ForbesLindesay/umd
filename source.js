5ee4b93be525ef2c2fc950a82c5f4fd8ed621662
Armando Meziat <amf-commits@mezi.at>
2015-11-24 00:25:52 -0200

    Allow specification of more than one name for the module
    This is mainly useful when exporting the module to the global
    namespace, e.g.: `umd(['jQuery', '$'], ...)` will yield `g.jQuery = g.$
    = f()`.

--- source.js
+++ source.js
@@ -3,13 +3,14 @@
 var fs = require('fs');
 var templateSTR = fs.readFileSync(__dirname + '/template.min.js', 'utf8');
 
-function template(moduleName, options) {
+function template(moduleNames, options) {
+  if (typeof moduleNames === 'string') moduleNames = [moduleNames];
   if (typeof options === 'boolean') {
     options = {commonJS: options};
   } else if (!options) {
     options = {};
   }
-  var str = templateSTR.replace(/defineNamespace\(\)/g, compileNamespace(moduleName))
+  var str = templateSTR.replace(/defineNamespace\(\)/g, compileModuleNames(moduleNames))
     .split('source()')
   str[0] = str[0].trim();
   //make sure these are undefined so as to not get confused if modules have inner UMD systems
@@ -30,11 +31,11 @@ exports = module.exports = function (name, src, options) {
   return exports.prelude(name, options) + src + exports.postlude(name, options);
 };
 
-exports.prelude = function (moduleName, options) {
-  return template(moduleName, options)[0];
+exports.prelude = function (moduleNames, options) {
+  return template(moduleNames, options)[0];
 };
-exports.postlude = function (moduleName, options) {
-  return template(moduleName, options)[1];
+exports.postlude = function (moduleNames, options) {
+  return template(moduleNames, options)[1];
 };
 
 
@@ -51,28 +52,30 @@ function camelCase(name) {
 }
 
 
+function compileModuleNames(moduleNames) {
+  return moduleNames.map(compileNamespace)
+                    .concat(['f()'])
+                    .join(' = ');
+}
+
 function compileNamespace(name) {
   var names = name.split('.')
 
   // No namespaces, yield the best case 'global.NAME = VALUE'
   if (names.length === 1) {
-    return 'g.' + camelCase(name) + ' = f()';
+    return 'g.' + camelCase(name);
 
   // Acceptable case, with reasonable compilation
   } else if (names.length === 2) {
     names = names.map(camelCase);
-    return '(g.' + names[0] + ' || (g.' + names[0] + ' = {})).' + names[1] + ' = f()';
+    return '(g.' + names[0] + ' || (g.' + names[0] + ' = {})).' + names[1];
 
   // Worst case, too many namespaces to care about
   } else {
-    var valueContainer = names.pop()
-    return names.map(compileNamespaceStep)
-                .concat(['g.' + camelCase(valueContainer) + ' = f()'])
-                .join(';');
+    names[0] = 'g.' + camelCase(names[0]);
+    return names.reduce(function (prev, current, index, array) {
+             array[index] = current = camelCase(current);
+             return '(' + prev + ' || (' + array.slice(0, index).join('.') + ' = {})).' + current;
+           });
   }
-}
-
-function compileNamespaceStep(name) {
-  name = camelCase(name);
-  return 'g=(g.' + name + '||(g.' + name + ' = {}))';
 }
